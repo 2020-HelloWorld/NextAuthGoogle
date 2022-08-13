cac0df2c92f24405244c19a4842f09f1de2ba2e6
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-08-13 20:04:55 +0530

    description page

---
+++ components/ideaPitching/Contacts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Contacts(props) {
+
+  return (
+    <div className='Contact-box'>
+        <div className='image'>
+            
+        </div>
+        <div className='contact-details'>
+            <lable>Email:</lable><a herf='#'>{}</a><br/>
+            <lable>Phone:</lable><a herf='#'>123456789</a><br/>
+            <lable>Address:</lable><a herf='#'>xyz,dgfs,cvds,2344325</a><br/>
+        </div>
+
+    </div>
+  )
+}
