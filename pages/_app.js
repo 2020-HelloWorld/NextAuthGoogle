beab8022aa431038ee22c0b77c69b26c4e037d5a
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-08-13 16:02:07 +0530

    Chart example testing with NextApp
    Signed-off-by: Ghanashyam-Bhat <ghanashyambhat6@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../components/pieChart.js'
+import '../components/lineChart.js'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
